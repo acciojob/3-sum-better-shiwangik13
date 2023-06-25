d4524897749254d668d232f56f42339f09d7aad8
shiwangik13 <85068938+shiwangik13@users.noreply.github.com>
2023-06-25 13:06:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let closetSum = Number.MAX_VALUE;
+
+	for(let i=0; i<arr.length; i++){
+		for(let j=i+1; j<arr.length; j++){
+			for(let k=j+1; k<arr.length; k++){
+				if(Math.abs(target - closetSum)> Math.abs(target - (arr[i]+arr[j]+arr[k]))){
+					closetSum = Math.abs(arr[i]+arr[j]+arr[k])
+				}
+			}
+		}
+	}
+	return closetSum
 }
 
 module.exports = threeSum;
